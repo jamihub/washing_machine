6cd52bfc06e659cde18e0637b06d52f80bf5fa55
Jami <nutgeroni@gmail.com>
2021-12-13 10:02:57 +0200

    init

---
+++ app.js
@@ -0,0 +1,81 @@
+/*
+12.12.2021
+Jami Seilonen 
+App name: Etäohjattava pesukone
+Description: Lähtötasotehtävä Bittiumille
+*/
+
+const express = require('express');              //expressin avulla voidaan käyttää HTTP requestejä.
+const app = express();
+
+let idle = new Boolean(true);
+let curState = 'Idle';
+
+function idleState() {
+    idle = true;        
+    curState = 'Idle';                           //curState on pesuohjelman vaiheen nimi.
+    console.log('Idle state');
+    return curState;
+}
+
+function step(phaseName) {                       //Step funktio luo pesukoneen vaiheet promisien avulla.
+    curState = phaseName;                           
+    return new Promise((resolve,reject)=>{       
+    if(idle == false){                           //Jos promise toteutuu pesukoneen tila siirtyy seuraavaan vaiheeseen.  
+        console.log(phaseName);
+        setTimeout(() => {
+        resolve('Going forward');               
+        },5*1000);
+    }
+    else{                                        //jos promise ei todeudu, tiedetään että käyttäjä on asettanut idlen tilan päälle eli pysäyttänyt koneen toiminnan.
+        reject('Going idle');
+        }
+    });
+}
+
+//pesukoneen pyöritys    
+async function runMachine(){   
+    idle = false;                                //Asetetaan idle tila falseksi että suoritus voidaan aloittaa.
+    try{                                         //awaitin avulla kone odottaa että stepin^ promise on täyttynyt ennen seuraavaan vaiheeseen siirtymistä.
+        await step('Filling Water');
+        await step('Rinsing');
+        await step('Spinning');
+        idleState(); 
+    }
+    catch(err){
+        console.log('Stopped by user');          //promisen toteutumatta jääminen asettaa koneen idle -tilaan.
+        idleState();
+    }
+}   
+
+//Komennot pesukoneen ohjaukseen
+    app.post('/api/start',(req,res) => {          //Start komennolla kysytään onko kone idle tilassa. Jos on niin sallitaan koneen käynnistys. 
+        if(idle==true){                                                          
+        res.status(200).json({message: 'Washing machine started'});
+        runMachine();
+        }                                          //Jos kone on käynnissä komento hylätään ja annetaan virheilmoitus.
+        else{                                         
+            res.status(400).json({message:'Can not start while running'});
+        }
+    });
+    
+    app.get('/api/status',(req,res) => {            //status komento palauttaa koneen pesuvaiheen nimen.
+        res.status(200).json({message: curState});
+    });
+
+    app.post('/api/abort',(req,res) => {            //abort komento muuttaa pesukoneen tilan idleksi.
+        res.status(200).json({message: 'Stopped by user'});
+        return idle = true;
+    });
+
+    //aloitaa idle -tilasta.
+    idleState();
+    
+    //Kuunnellaan porttia 3000.
+    app.listen(3000, () => {console.log('3000')});
+
+    module.exports.app = app;
+    module.exports.funcs = {
+        idleState: idleState,
+        step: step
+    }
---
+++ test/appTest.spec.js
@@ -0,0 +1,51 @@
+const assert = require ('assert');
+const funcs = require('../app.js').funcs; 
+const app = require('../app.js').app; 
+const supertest = require('supertest');
+
+describe('App', () => {
+    it('idleState should return idle', function(){
+        let result = funcs.idleState();
+        assert.equal(result, 'Idle');
+    });
+    it('idleState should be a string', function(){
+        let result = funcs.idleState();
+        assert.toString(result, 'Idle');
+    });
+});
+
+describe('GET /api/status', function() {
+    it('Should has status code 200', function(done) {
+      supertest(app)
+        .get('/api/status')
+        .expect(200)
+        .end(function(err, res){
+          if (err) done(err);
+          done();
+        });
+    });
+});
+
+describe('POST /api/start', function done() {
+it('Machine start successful', async () => {
+    supertest(app)
+     .post('/api/start')
+     .expect(200)
+     .end(function(err, res) {
+        if (err) done(err);
+     });
+     done();
+   });
+});
+
+describe('POST /api/abort', function done() {
+    it('Washing aborted', async () => {
+        supertest(app)
+         .post('/api/abort')
+         .expect(200)
+         .end(function(err, res) {
+            if (err) done(err);
+         });
+         done();
+       });
+    });
